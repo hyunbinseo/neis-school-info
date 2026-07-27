3e40f673a4ae8e23816d425c1be9180f49139aa1
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-07-27 18:36:49 +0900

    chore: bump deps, use type-aware linting

--- src/search.test.ts
+++ src/search.test.ts
@@ -6,7 +6,7 @@ import { search } from './search.ts';
 const { NEIS_API_KEY: apiKey } = env;
 if (!apiKey) throw new Error();
 
-test('기본 검색', async () => {
+void test('기본 검색', async () => {
 	const result = await search({ filters: { 학교명: '가락고등학교' } }, { apiKey });
 	assert.equal(result.ok, true);
 	assert.equal(result.code, 'INFO-000');
@@ -17,7 +17,7 @@ test('기본 검색', async () => {
 	assert.equal(school.학교명, '가락고등학교');
 });
 
-test('검색 시 지정한 필드만 반환', async () => {
+void test('검색 시 지정한 필드만 반환', async () => {
 	const result = await search(
 		{
 			filters: { 학교명: '가락고등학교' },
@@ -32,12 +32,12 @@ test('검색 시 지정한 필드만 반환', async () => {
 	assert.deepEqual(Object.keys(school).sort(), ['전화번호', '학교명']);
 });
 
-test('검색 결과가 없는 경우', async () => {
+void test('검색 결과가 없는 경우', async () => {
 	const result = await search({ filters: { 학교명: 'invalid' } }, { apiKey });
 	assert.deepEqual(result, { ok: true, code: 'INFO-200', totalCount: 0, schools: [] });
 });
 
-test('잘못된 API 키를 입력한 경우', async () => {
+void test('잘못된 API 키를 입력한 경우', async () => {
 	const result = await search({}, { apiKey: 'invalid' });
 	assert.equal(result.ok, false);
 	assert.equal(result.code, 'ERROR-290');
