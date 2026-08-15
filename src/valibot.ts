d808ea60541d35376ab7c10e02f23296aa9c3333
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-08-16 07:50:29 +0900

    chore: normalize duplicate whitespaces

--- src/valibot.ts
+++ src/valibot.ts
@@ -60,11 +60,16 @@ export const RawSchoolSchema = pipe(
 	),
 );
 
-const TrimmedNonEmptyStringSchema = pipe(string(), trim(), nonEmpty());
-
-const TrimmedStringEmptyToNullSchema = pipe(
+const NormalizeStringSchema = pipe(
 	string(),
+	transform((v: string) => v.replaceAll(/\s+/g, ' ')), // e.g. NBSP in 'Kangnam Elementary School'
 	trim(),
+);
+
+const NonEmptyStringSchema = pipe(NormalizeStringSchema, nonEmpty());
+
+const StringEmptyToNullSchema = pipe(
+	NormalizeStringSchema,
 	transform((v) => v || null),
 );
 
@@ -72,7 +77,7 @@ const TrimmedStringEmptyToNullSchema = pipe(
 const PhoneNumberSchema = fallback(
 	nullable(
 		pipe(
-			TrimmedNonEmptyStringSchema,
+			NonEmptyStringSchema,
 			transform((v) => v.replaceAll(/[() .+-]/g, '')), // e.g. 054--475-5386, +82-2-1234-5678
 			digits(),
 			minLength(8), // e.g. 15995789; intentionally drops 032114, etc.
@@ -85,7 +90,7 @@ const PhoneNumberSchema = fallback(
 const URLSchema = fallback(
 	nullable(
 		pipe(
-			TrimmedNonEmptyStringSchema, // e.g. leading BOM in '﻿﻿http://school.cbe.go.kr/gagok-e/'
+			NonEmptyStringSchema, // e.g. leading BOM in '﻿﻿http://school.cbe.go.kr/gagok-e/'
 			transform((v) => v.replaceAll(' ', '')), // e.g. 'http://ya-seochang. jge.es.kr'
 			// NOTE A public domain has a dot (name.tld); rare exceptions can be ignored
 			check((v) => v.includes('.')), // e.g. '강원도 정선군 여량면 여량7길 42' is not a URL
@@ -144,29 +149,27 @@ const 주야구분명 = ['주간', '야간', '주야간'] as const;
 export type School = InferOutput<typeof SchoolSchema>;
 export const SchoolSchema = object({
 	시도교육청코드: picklist(OFFICE_CODES),
-	시도교육청명: TrimmedNonEmptyStringSchema,
-	행정표준코드: TrimmedStringEmptyToNullSchema,
-	학교명: TrimmedNonEmptyStringSchema,
-	영문학교명: nullable(TrimmedStringEmptyToNullSchema),
-	학교종류명: nullable(pipe(TrimmedNonEmptyStringSchema, picklist(학교종류명))),
-	시도명: TrimmedNonEmptyStringSchema,
-	관할조직명: TrimmedNonEmptyStringSchema,
-	설립명: nullable(pipe(TrimmedNonEmptyStringSchema, picklist(설립명))),
+	시도교육청명: NonEmptyStringSchema,
+	행정표준코드: StringEmptyToNullSchema,
+	학교명: NonEmptyStringSchema,
+	영문학교명: nullable(StringEmptyToNullSchema),
+	학교종류명: nullable(pipe(NonEmptyStringSchema, picklist(학교종류명))),
+	시도명: NonEmptyStringSchema,
+	관할조직명: NonEmptyStringSchema,
+	설립명: nullable(pipe(NonEmptyStringSchema, picklist(설립명))),
 	도로명우편번호: fallback(nullable(pipe(string(), trim(), digits(), length(5))), null),
-	도로명주소: nullable(TrimmedNonEmptyStringSchema),
-	도로명상세주소: nullable(TrimmedStringEmptyToNullSchema),
+	도로명주소: nullable(NonEmptyStringSchema),
+	도로명상세주소: nullable(StringEmptyToNullSchema),
 	전화번호: PhoneNumberSchema,
 	홈페이지주소: URLSchema,
-	남녀공학구분명: pipe(TrimmedNonEmptyStringSchema, picklist(남녀공학구분명)),
+	남녀공학구분명: pipe(NonEmptyStringSchema, picklist(남녀공학구분명)),
 	팩스번호: PhoneNumberSchema,
-	고등학교구분명: nullable(pipe(TrimmedNonEmptyStringSchema, picklist(고등학교구분명))),
+	고등학교구분명: nullable(pipe(NonEmptyStringSchema, picklist(고등학교구분명))),
 	산업체특별학급존재여부: picklist(['N', 'Y']),
-	고등학교일반전문구분명: nullable(
-		pipe(TrimmedNonEmptyStringSchema, picklist(고등학교일반전문구분명)),
-	),
-	특수목적고등학교계열명: nullable(TrimmedNonEmptyStringSchema),
-	입시전후기구분명: pipe(TrimmedNonEmptyStringSchema, picklist(입시전후기구분명)),
-	주야구분명: pipe(TrimmedNonEmptyStringSchema, picklist(주야구분명)),
+	고등학교일반전문구분명: nullable(pipe(NonEmptyStringSchema, picklist(고등학교일반전문구분명))),
+	특수목적고등학교계열명: nullable(NonEmptyStringSchema),
+	입시전후기구분명: pipe(NonEmptyStringSchema, picklist(입시전후기구분명)),
+	주야구분명: pipe(NonEmptyStringSchema, picklist(주야구분명)),
 	설립일자: YYYYMMDDToISODateSchema,
 	개교기념일: YYYYMMDDToISODateSchema,
 	수정일자: YYYYMMDDToISODateSchema,
